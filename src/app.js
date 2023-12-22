dccee9aff853e65d1e02a8aaa119e54ad0e760e2
aoch <andrew.och@gmail.com>
2023-12-22 16:16:50 +0000

    Adding quick sort with a pivot of half the array

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-import { insertionSort } from './sorting.js';
+import {
+    insertionSort,
+    quickSort
+} from './sorting.js';
 
 const testInput = [8, 1, 9, 7, 2, 4];
-console.log(insertionSort(testInput));
+console.log(insertionSort([...testInput]));
+console.log(quickSort([...testInput]));
--- src/sorting.js
+++ src/sorting.js
@@ -1,4 +1,7 @@
 const insertionSort = (arr) => {
+    if (!Array.isArray(arr))
+        return [];
+
     for (let i = 1; i < arr.length; i++) {
         let currentValue = arr[i];
         let j;
@@ -11,4 +14,29 @@ const insertionSort = (arr) => {
     return arr;
 };
 
-export { insertionSort };
+const quickSort = (arr) => {
+    if (!Array.isArray(arr))
+        return [];
+
+    if (arr.length < 2)
+        return arr;
+
+    const pivotIndex = Math.floor(arr.length / 2);
+    const pivotValue = arr[pivotIndex];
+    const smaller = [];
+    const greater = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i === pivotIndex)
+            continue;
+
+        arr[i] <= pivotValue ? smaller.push(arr[i]) : greater.push(arr[i]);
+    }
+
+    return [].concat(quickSort(smaller), [pivotValue], quickSort(greater));
+};
+
+export {
+    insertionSort,
+    quickSort
+};
